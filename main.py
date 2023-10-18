89caad955e3bc007020c76ff81bf9ea7d5a410c3
AG4lyf <surajbhari159@gmail.com>
2023-10-18 16:10:59 +0530

    wait for discord connection if discord is not on

--- main.py
+++ main.py
@@ -5,33 +5,44 @@ from get_info import get_media_info
 
 
 client_id = "1163238681088364584"  # Replace this with your own client id
+icon_name = "musical-note"
 last_track = None
 
 def is_playing(media_info):
     return int(media_info['playback_status']) == 4
 
 if __name__ == '__main__':
-    with Presence(client_id) as presence:
-        while True:
-            time.sleep(2)
-            current_media_info = asyncio.run(get_media_info())
-            if not any([current_media_info, is_playing(current_media_info)]):
-                print("No media playing")
-                presence.clear()
-                last_track = None
-                continue
-            if last_track == current_media_info['title']:
-                continue
-            presence.set(
-                {
-                    "state": current_media_info['artist'],
-                    "details": current_media_info['title'],
-                    "timestamps": {
-                        "start": int(time.time())
-                    },
-                    "assets": {
-                        "large_image": "musical-note"
-                    }
+    while True:
+        try:
+            presence = Presence(client_id)
+        except FileNotFoundError:
+            time.sleep(5)
+            continue
+        else:
+            print(f"Connected to Discord")
+            break
+        
+    while True:
+        time.sleep(2)
+        current_media_info=get_media_info()
+        if not any([current_media_info, is_playing(current_media_info)]):
+            print("No media playing")
+            presence.clear()
+            last_track = None
+            continue
+        if last_track == current_media_info['title']:
+            continue
+        end_time = current_media_info["end_time"] - current_media_info['start_time']
+        presence.set(
+            {
+                "state": current_media_info['artist'],
+                "details": current_media_info['title'],
+                "timestamps": {
+                    "end": end_time.seconds, 
+                },
+                "assets": {
+                    "large_image": icon_name
                 }
-            )
-            last_track = current_media_info['title']
+            }
+        )
+        last_track = current_media_info['title']
