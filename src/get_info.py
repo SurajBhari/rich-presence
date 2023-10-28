eb163e6bbb37fc85b243389666b9630a15ec02ed
AG4lyf <surajbhari159@gmail.com>
2023-10-28 14:23:08 +0530

    fix empty searching error

--- src/get_info.py
+++ src/get_info.py
@@ -19,39 +19,36 @@ async def _get_media_info():
     session = sessions.get_current_session()
     if not session:
         return None
-    TARGET_ID = session.source_app_user_model_id
-
     if session:  # there needs to be a media session running
-        if session.source_app_user_model_id == TARGET_ID:
-            pinfo = session.get_playback_info()
-            info = await session.try_get_media_properties_async()
-            timeline = session.get_timeline_properties()
-            # song_attr[0] != '_' ignores system attributes
-            info_dict = {
-                "artist": info.artist,
-                "title": info.title,
-                "genres": info.genres,
-                "playback_status": pinfo.playback_status,
-                "duration": pinfo.playback_type,
-                "end_time": timeline.end_time,
-                "last_updated": timeline.last_updated_time,
-                "max_seek": timeline.max_seek_time,
-                "min_seek": timeline.min_seek_time,
-                "position": timeline.position,
-                "start_time": timeline.start_time
-            }
-            search = yt.search(f'{info.title} {info.artist}', filter="songs", limit=1)
-            if info.title not in search[0]['title']:
-                thumbnail = "musical-note"
-                link = ""
-            else:
+        pinfo = session.get_playback_info()
+        info = await session.try_get_media_properties_async()
+        timeline = session.get_timeline_properties()
+        # song_attr[0] != '_' ignores system attributes
+        info_dict = {
+            "artist": info.artist,
+            "title": info.title,
+            "genres": info.genres,
+            "playback_status": pinfo.playback_status,
+            "duration": pinfo.playback_type,
+            "end_time": timeline.end_time,
+            "last_updated": timeline.last_updated_time,
+            "max_seek": timeline.max_seek_time,
+            "min_seek": timeline.min_seek_time,
+            "position": timeline.position,
+            "start_time": timeline.start_time
+        }
+        search = yt.search(f'{info.title} {info.artist}', filter="songs", limit=1)
+        thumbnail = "musical-note"
+        link = ""
+        if search:
+            if info.title in search[0]['title']:
                 thumbnail = search[0]['thumbnails'][-1]['url']
                 link = f"https://music.youtube.com/watch?v={search[0]['videoId']}"
             info_dict['thumbnail'] = thumbnail
-            info_dict['link'] = link
-            # converts winrt vector to list
-            info_dict['genres'] = list(info_dict['genres'])
-            return info_dict
+        info_dict['link'] = link
+        # converts winrt vector to list
+        info_dict['genres'] = list(info_dict['genres'])
+        return info_dict
     raise Exception('TARGET_PROGRAM is not the current media session')
 
 
