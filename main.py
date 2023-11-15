439238b61f75f6969e62905e6817fe2d7f25f841
AG4lyf <surajbhari159@gmail.com>
2023-11-16 02:08:08 +0530

    take care if the song is new song isntead of unpaused to count as new

--- main.py
+++ main.py
@@ -38,88 +38,95 @@ def download_song(url, output_folder="."):
         ydl.download([url])
 
 
-if __name__ == '__main__':
-    presence = get_presence()
-    while True:
-        time.sleep(2)
-        current_media_info=get_media_info()
-        if not current_media_info or not is_playing(current_media_info) or not current_media_info['artist']:
-            if last_track: # we should not spam the clear function if there was no song previously played
-                print("No media playing, Cleaning up Presence.")
-                try:
-                    presence.clear()
-                except OSError:
-                    print("Discord have stopped responding")
-                    presence = get_presence()
-                    continue
-                last_track = None
-            continue
-        if last_track == current_media_info['title']:
-            continue
-        end_time = current_media_info["end_time"] - current_media_info['position']
-        start = int(time.time())
-        end = int(time.time()) + end_time.seconds
-        presence_data = {
-            "state": current_media_info['artist'], # Note: This is the artist that is taken from windows. `artists` have more than one artist taken from yt
-            "details": current_media_info['title'],
-            "timestamps": {
-                "start": start,
-                "end": end, 
-            },
-            "assets": {
-                "large_image": current_media_info['thumbnail'] or "https://media.tenor.com/15YUsMWt4FEAAAAi/music.gif",
-            }
+if __name__ != '__main__':
+    exit(-1) # this file should not be imported
+
+
+
+presence = get_presence()
+
+while True:
+    time.sleep(2)
+    current_media_info=get_media_info()
+    if not current_media_info or not is_playing(current_media_info) or not current_media_info['artist']:
+        if last_track: # we should not spam the clear function if there was no song previously played
+            print("No media playing, Cleaning up Presence.")
+            try:
+                presence.clear()
+            except OSError:
+                print("Discord have stopped responding")
+                presence = get_presence()
+                continue
+            last_track = None
+        continue
+    if last_track == current_media_info['title']:
+        continue
+    end_time = current_media_info["end_time"] - current_media_info['position']
+    start = int(time.time())
+    end = int(time.time()) + end_time.seconds
+    presence_data = {
+        "state": current_media_info['artist'], # Note: This is the artist that is taken from windows. `artists` have more than one artist taken from yt
+        "details": current_media_info['title'],
+        "timestamps": {
+            "start": start,
+            "end": end, 
+        },
+        "assets": {
+            "large_image": current_media_info['thumbnail'] or "https://media.tenor.com/15YUsMWt4FEAAAAi/music.gif",
         }
-        if end-start > 3590:
-            del presence_data["timestamps"]
-        if current_media_info["link"]: 
-            presence_data["buttons"] = [
-                {
-                    "label": "Listen on YouTube",
-                    "url": current_media_info["link"]
-                }
-            ]
-        try:
-            presence.set(presence_data)
-        except OSError:
-            print("Discord have stopped responding")
-            presence = get_presence()
-        print(f"Changed presence info to {current_media_info['artist']} - {current_media_info['title']}")
-        last_track = current_media_info['title']
-        # download the song 
-        drp = f"{music_folder}/drp"
-        if "drp" not in os.listdir(music_folder):
-            os.makedirs(music_folder+"/drp")
-        if "drp.json" not in os.listdir(music_folder+"/drp"):
-            with open(music_folder+"/drp/drp.json", "w+") as f:
-                json.dump({}, f)
-        with open(music_folder+"/drp/drp.json", "r") as f:
-            data = json.load(f)
-        
-        if current_media_info['id'] in data.keys():
+    }
+    if end-start > 3590:
+        del presence_data["timestamps"] # for livestreams windows tell that there is 1 hour left. but it is not true
+    if current_media_info["link"]: 
+        presence_data["buttons"] = [
+            {
+                "label": "Listen on YouTube",
+                "url": current_media_info["link"]
+            }
+        ]
+    try:
+        presence.set(presence_data)
+    except OSError:
+        print("Discord have stopped responding")
+        presence = get_presence()
+    print(f"Changed presence info to {current_media_info['artist']} - {current_media_info['title']}")
+    last_track = current_media_info['title']
+    drp = f"{music_folder}/drp"
+    if "drp" not in os.listdir(music_folder):
+        os.makedirs(music_folder+"/drp")
+    if "drp.json" not in os.listdir(music_folder+"/drp"):
+        with open(music_folder+"/drp/drp.json", "w+") as f:
+            json.dump({}, f)
+    with open(music_folder+"/drp/drp.json", "r") as f:
+        data = json.load(f)
+    
+    if current_media_info['id'] in data.keys():
+        if current_media_info['position'].seconds < 5:
+            # it means that the current song is just started and not resumed.
             data[current_media_info['id']]['count'] += 1 
             data[current_media_info['id']]['time'].append(time.time())
-        else:
-            data[current_media_info['id']] = {
-                "count": 1,
-                "title": current_media_info['title'],
-                "artist": current_media_info['artist'],
-                "link": current_media_info['link'],
-                "thumbnail": current_media_info['thumbnail'],
-                "time": [time.time()]
-            }
-        # I tried to get the genre info too. but its not directly given
-        with open(music_folder+"/drp/drp.json", "w") as f:
-            json.dump(data, f, indent=4)
-        if f"{current_media_info['artist']} {current_media_info['title']}.mp3" in os.listdir(drp):
-            continue
-        if not download_songs:
-            continue
-        if not current_media_info['link']:
-            continue
-        try:
-            download_song(current_media_info['link'], drp)
-        except Exception as e:
-            print(e)
-            continue
+    else:
+        data[current_media_info['id']] = {
+            "count": 1,
+            "title": current_media_info['title'],
+            "artist": current_media_info['artist'],
+            "link": current_media_info['link'],
+            "thumbnail": current_media_info['thumbnail'],
+            "time": [time.time()]
+        }
+    # I tried to get the genre info too. but its not directly given
+    with open(music_folder+"/drp/drp.json", "w") as f:
+        json.dump(data, f, indent=4)
+    # download the song 
+    if f"{current_media_info['artist']} {current_media_info['title']}.mp3" in os.listdir(drp):
+        continue
+    if not download_songs:
+        continue
+    if not current_media_info['link']:
+        continue
+    try:
+        download_song(current_media_info['link'], drp)
+    except Exception as e:
+        print(e)
+        continue
         
